1f787f20cc84ef75d83dfcc34860dfa87528ec3d
Collins Oduor <oduorotieno1304@gmail.com>
2022-03-29 07:11:12 +0300

    add Button and Picks components

---
+++ components/Button.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #000000;
+  background-color: #ffffff;
+  border: 2px solid #ffffff;
+  border-radius: 5px;
+  padding: 1%;
+  height: fit-content;
+`;
---
+++ components/Picks.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Picks = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  column-gap: 5vw;
+  height: 50vh;
+`;
