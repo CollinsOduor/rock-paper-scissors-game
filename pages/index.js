6cf9e4b497a1419fac0ac84b9cf28cdd36743590
Collins Oduor <oduorotieno1304@gmail.com>
2022-03-29 06:48:09 +0300

    add game logic

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,50 @@
 import Head from "next/head";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { Button } from "../components/Button";
 import { GameBoard } from "../components/GameBoard";
 import { Heading } from "../components/Heading";
 import { List } from "../components/List";
 import { ListItem } from "../components/ListItem";
 import { MainBody } from "../components/MainBody";
 import { Overlay } from "../components/Overlay";
+import { Picks } from "../components/Picks";
 import { Scoresheet } from "../components/Scoresheet";
 
 export default function Home() {
   const items = [
-    { key: "rock", value: "ROCK" },
-    { key: "paper", value: "PAPER" },
-    { key: "scissors", value: "SCISSORS" },
+    { key: "rock", value: "ROCK", image: "/icon-rock.svg", color: "#DE465A" },
+    {
+      key: "paper",
+      value: "PAPER",
+      image: "/icon-paper.svg",
+      color: "#5671F4",
+    },
+    {
+      key: "scissors",
+      value: "SCISSORS",
+      image: "/icon-scissors.svg",
+      color: "#EEA52E",
+    },
   ];
+
+  //cache who beats who
+  const rules = { paper: "rock", rock: "scissors", scissors: "paper" };
   const [showOverlay, setShowOverlay] = useState(false);
+  const [housePick, setHousePick] = useState(null);
+  const [pick, setPick] = useState(null);
+  const [outcome, setOutcome] = useState("");
+  const [score, setScore] = useState(0);
+
+  useEffect(() => {
+    if (rules[pick?.key] === housePick?.key) {
+      setScore((previous) => previous + 1);
+    }
+    if (rules[housePick?.key] === pick?.key) {
+      {
+        setScore((previous) => previous - 1);
+      }
+    }
+  }, [housePick]);
   return (
     <>
       <Head>
@@ -35,61 +65,122 @@ export default function Home() {
 
           <Scoresheet>
             <div style={{ color: "#555EAB" }}>SCORE</div>
-            <div style={{ color: "#5B596E" }}>12</div>
+            <div style={{ color: "#5B596E" }}>{score}</div>
           </Scoresheet>
         </Heading>
-        <GameBoard>
-          <div
-            style={{
-              gridArea: "paper",
-              border: "10px solid #5671F4",
-              borderRadius: "50%",
-              width: "100px",
-              height: "100px",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <img src="/icon-paper.svg" alt="Paper" />
-          </div>
-          <div
-            style={{
-              gridArea: "scissors",
-              border: "10px solid #EEA52E",
-              borderRadius: "50%",
-              width: "100px",
-              height: "100px",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <img src="/icon-scissors.svg" alt="Scissors" />
-          </div>
-          <div
-            style={{
-              gridArea: "rock",
-              border: "10px solid #DE465A",
-              borderRadius: "50%",
-              width: "100px",
-              height: "100px",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <img src="/icon-rock.svg" alt="Rock" />
-          </div>
-        </GameBoard>
-        <Scoresheet
+        {pick === null && (
+          <GameBoard>
+            {items.map((item) => (
+              <div
+                key={item.key}
+                style={{
+                  gridArea: `${item.key}`,
+                  border: `10px solid ${item.color}`,
+                  borderRadius: "50%",
+                  width: "100px",
+                  height: "100px",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  cursor: "pointer",
+                }}
+                onClick={() => {
+                  setPick(item);
+                  setHousePick(items[Math.floor(Math.random() * items.length)]);
+                }}
+              >
+                <img src={item.image} alt={item.key} />
+              </div>
+            ))}
+          </GameBoard>
+        )}
+
+        {pick !== null && (
+          <Picks>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                rowGap: "2vh",
+              }}
+            >
+              You picked
+              <div
+                style={{
+                  border: `10px solid ${pick.color}`,
+                  borderRadius: "50%",
+                  width: "100px",
+                  height: "100px",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <img src={pick.image} alt={pick.key} />
+              </div>
+            </div>
+
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                rowGap: "2vh",
+              }}
+            >
+              You{" "}
+              {pick.key === housePick.key
+                ? "drew"
+                : rules[pick.key] === housePick.key
+                ? "won"
+                : "lost"}
+              <Button
+                style={{ cursor: "pointer" }}
+                onClick={() => {
+                  setPick(null);
+                }}
+              >
+                Play Again
+              </Button>
+            </div>
+
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                rowGap: "2vh",
+              }}
+            >
+              The House picked
+              <div
+                style={{
+                  border: `10px solid ${housePick.color}`,
+                  borderRadius: "50%",
+                  width: "100px",
+                  height: "100px",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <img src={housePick.image} alt={housePick.key} />
+              </div>
+            </div>
+          </Picks>
+        )}
+        <Button
           style={{ justifySelf: "end", cursor: "pointer" }}
           onClick={() => {
             setShowOverlay((curr) => !curr);
           }}
         >
           RULES
-        </Scoresheet>
+        </Button>
 
         {showOverlay && (
           <Overlay>
